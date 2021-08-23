ce579fe0c14dea2ef3be889ecb45f406be393d12
Ignacio Giadach <igiadach@gmail.com>
2021-08-23 16:20:42 +1000

    connected to db

--- app.js
+++ app.js
@@ -9,4 +9,22 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+const dbConn = "mongodb://localhost/rmc"
+
+mongoose.connect(
+    dbConn,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true
+    },
+    err => {
+        if (err){
+            console.log("error connecting database", err)
+        } else {
+            console.log("connected to database")
+        }
+     }
+)
+
 app.listen(port, ()=> console.log(`server running on port ${port}`))
