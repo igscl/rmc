39ca34f812ec996a986b666b310ccb4206efa8dd
Ignacio Giadach <igiadach@gmail.com>
2021-10-24 21:28:38 +1100

    cors headers

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ app.use(session({
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://rmc-planeta.netlify.app")
     res.header("Access-Control-Allow-Credentials", true)
+    res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS')
     res.header("Access-Control-Allow-Headers", "X-Requested-With")
     next()
   })
