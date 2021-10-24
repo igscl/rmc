c24893774f04611392cb040c0b2d7db48e8d0211
Ignacio Giadach <igiadach@gmail.com>
2021-10-24 19:38:11 +1100

    cors

--- app.js
+++ app.js
@@ -73,6 +73,12 @@ app.use(session({
     })
 }))
 
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    next()
+  })
+
 require('./config/passport')
 app.use(passport.initialize())
 app.use(passport.session())
